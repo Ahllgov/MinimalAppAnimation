9cb056f3af514c3841a94ac33ae783ee88d394d9
makhilgov <makhilgov@ozon.ru>
2022-08-12 11:55:23 +0300

    Initial Commit

---
+++ MinimalAppAnimation/MinimalAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalAppAnimationApp.swift
+//  MinimalAppAnimation
+//
+//  Created by Akhilgov Magomed Abdulmazhitovich on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MinimalAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
